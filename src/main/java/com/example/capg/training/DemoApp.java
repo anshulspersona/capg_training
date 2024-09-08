5f29cddc3d1237b63fd28d3ed88a41ba15e3d224
aakashsr <anshul.anshul.sri@gmail.com>
2024-09-08 19:59:11 +0530

    Test commit

--- src/main/java/com/example/capg/training/DemoApp.java
+++ src/main/java/com/example/capg/training/DemoApp.java
@@ -7,6 +7,6 @@ public class DemoApp {
 
     public static void main(String[] args) {
         DemoApp app = new DemoApp();
-        System.out.println("Sum of 5 and 3 is: " + app.addNumbers(5, 3));
+        System.out.println("Sum of 10 and 3 is: " + app.addNumbers(5, 3));
     }
 }
